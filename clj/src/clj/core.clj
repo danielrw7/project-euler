9b6cf39d6f4a169309d5383830b2781209ef121c
Daniel Wilson <danielrw7@gmail.com>
2016-03-22 12:32:18 -0400

    Problem loader for command line

--- clj/src/clj/core.clj
+++ clj/src/clj/core.clj
@@ -1,9 +1,29 @@
 (ns clj.core
-  (:gen-class)
-  (require [clj.problem1 :as problem1])
-  (require [clj.problem2 :as problem2]))
+  (:gen-class))
+
+(def filePrefix "problem")
+(defn moduleFileName [n] (str "clj/" filePrefix n))
+(defn moduleName [n] (str "clj." filePrefix n))
+(defn fileName [n] (str "src/clj/" filePrefix n ".clj"))
+
+(defn existingProblems
+  []
+  (take-while
+    #(.exists (clojure.java.io/as-file (fileName %)))
+    (iterate inc 1)))
+
+(defn loadAndInit
+  "Loads a problem file by n"
+  [& args]
+  (doseq
+    [n args]
+    (load (moduleFileName n))
+    (apply
+      (ns-resolve
+        (symbol (moduleName n))
+        (symbol "-main"))
+      [])))
 
 (defn -main
-  [& args]
-  (problem1/-main)
-  (problem2/-main))
+  ([]       (apply loadAndInit (existingProblems)))
+  ([& args] (apply loadAndInit args)))
